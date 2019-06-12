a2bc4674649d6f80644501577a3ea75c49bbbe9f
john <732097792@qq.com>
2019-06-12 22:56:58 +0800

    完善测试代码

--- threadnotify/src/test/java/arrayblockqueue/ArrayQueueTest.java
+++ threadnotify/src/test/java/arrayblockqueue/ArrayQueueTest.java
@@ -10,19 +10,14 @@ public class ArrayQueueTest {
         }).start();
 
         new Thread(() -> {
-            stringArrayQueue.put(1);
+            stringArrayQueue.put(2);
         }).start();
 
         new Thread(() -> {
-            stringArrayQueue.put(1);
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            stringArrayQueue.put(3);
         }).start();
 
-        //队列已满，需要等待消费空出才能插入
+        //队列已满，等待消费了队列有空间，通知等待fullLock的线程
         new Thread(() -> {
             long start = System.currentTimeMillis();
             stringArrayQueue.put(1);
@@ -30,7 +25,58 @@ public class ArrayQueueTest {
             System.out.println("wait to put"+end);
         }).start();
 
+        //模拟消费场景
         try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //消费了队列有空间，会通知等待fullLock的put线程进行put操作
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName()+"消费："+ stringArrayQueue.get());
+        }).start();
+    }
+
+    @org.junit.Test
+    public void get() {
+        final ArrayQueue<Integer> stringArrayQueue = new ArrayQueue<>(3);
+        stringArrayQueue.put(1);
+        stringArrayQueue.put(2);
+        stringArrayQueue.put(3);
+
+        new Thread(() -> {
+            System.out.println(Thread.currentThread()+" get : "+stringArrayQueue.get());
+        }).start();
+
+        new Thread(() -> {
+            System.out.println(Thread.currentThread()+" get : "+stringArrayQueue.get());
+        }).start();
+
+        new Thread(() -> {
+            System.out.println(Thread.currentThread()+" get : "+stringArrayQueue.get());
+        }).start();
+
+        //队列已空，需要等待生产，通知等待emptyLock的线程去消费
+        new Thread(() -> {
+            long start = System.currentTimeMillis();
+            System.out.println(Thread.currentThread()+" get : "+stringArrayQueue.get());
+            long end = System.currentTimeMillis() - start;
+            System.out.println("wait to get"+end);
+        }).start();
+
+        //模拟生产时间
+        try {
+            Thread.sleep(5000);
+            //stringArrayQueue.put(6);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //这里生产数据，通知等待emptyLock的get线程
+        stringArrayQueue.put(6666);
+
+        /*try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -38,13 +84,8 @@ public class ArrayQueueTest {
 
         //消费了，会通知put线程进行put操作
         new Thread(() -> {
-            System.out.println(Thread.currentThread().getName()+"消费："+String.valueOf(stringArrayQueue.get()));
-        }).start();
-    }
-
-    @org.junit.Test
-    public void get() {
-
+            System.out.println(Thread.currentThread().getName()+"消费："+ stringArrayQueue.get());
+        }).start();*/
 
     }
 }
